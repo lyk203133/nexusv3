280cc4e2bbff5f16ce3892fa19187a02c97f231e
lucas <lyk20313@gmail.com>
2026-03-19 16:49:25 +0800

    收銀台日期調整

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -229,7 +229,9 @@ function formatDateTime(dateString) {
   const date = new Date(dateString)
   const day = date.getDate().toString().padStart(2, '0')
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  return `${day}/${month}`
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  return `${day}/${month} ${hours}:${minutes}`
 }
 
 function getStatusColor(status) {
